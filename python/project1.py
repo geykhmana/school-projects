f615ccb888dd8f75376356f3c824723c580607e1
Alan Geykhman <alangeykhman@Alans-MacBook-Pro.local>
2022-10-20 10:05:47 -0400

    Added Project 1

--- python/project1.py
+++ python/project1.py
@@ -1,10 +1,20 @@
 import random
 
 def playgame():
-    num = random.randint(1, 50)
+    randomNumber = random.randint(1, 50)
+    numberOfGuesses = 1
+    playAgain = 1
     print("Guess a number between 1 and 50")
     i = 1
-    guess1 = int(input(""))
-    if guess1 == num:
-        print("Correct! You guessed the number in", i, "attempts. Great job!")
-
+    while(numberOfGuesses < 11):
+        guess = int(input("Enter the number that you think is correct: "))
+        if (guess == randomNumber):
+            print("Congratulations, you guessed the correct number randomNumber in", numberOfGuesses, "guesses!\n")
+            break
+        elif(guess < randomNumber):
+            print("Your guess of", guess, "was too low")
+        elif (guess > randomNumber):
+            print("Your guess of", guess, "was too high")
+        numberOfGuesses += 1;
+        if (numberOfGuesses == GUESSES):
+            print("You did not guess the right number. The correct number was", randomNumber)
