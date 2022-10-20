b5707fc88db55b3c986cfeabd72e0b64907f8029
Alan Geykhman <alangeykhman@Alans-MacBook-Pro.local>
2022-10-20 12:11:21 -0400

    Updating project1

--- python/project1.py
+++ python/project1.py
@@ -1,16 +1,29 @@
 import random
 
-def playgame():
+def playAgain():
+    answer = input("Would you like to play again? Type Y for yes or N for no.")
+    if answer == "Y":
+        return 1
+    elif answer == "N":
+        print("Thanks for playing!")
+        return 0
+    else:
+        print("Error: your input was invalid.\n")
+        playAgain()
+
+def playGame():
     randomNumber = random.randint(1, 50)
-    numberOfGuesses = 1
-    playAgain = 1
+    numberOfGuesses = 0
     print("Guess a number between 1 and 50")
     i = 1
     while(numberOfGuesses < 11):
         guess = int(input("Enter the number that you think is correct: "))
         if (guess == randomNumber):
             print("Congratulations, you guessed the correct number", randomNumber, "in", numberOfGuesses, "guesses!")
-            break
+            numberOfGuesses = 0
+            response = playAgain()
+            if response == 0:
+                break
         elif(guess < randomNumber):
             print("Your guess of", guess, "was too low")
         elif (guess > randomNumber):
@@ -18,5 +31,9 @@ def playgame():
         numberOfGuesses += 1;
         if (numberOfGuesses == 10):
             print("You did not guess the right number. The correct number was", randomNumber)
+            numberOfGuesses = 0
+            response = playAgain()
+            if response == 0:
+                break
 
-playgame()
+playGame()
