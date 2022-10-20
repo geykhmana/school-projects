37e69f6673ae0475e035dd112b2f7698e0774008
Alan Geykhman <alangeykhman@Alans-MacBook-Pro.local>
2022-10-20 12:46:23 -0400

    Latest version of project1

--- python/project1.py
+++ python/project1.py
@@ -1,15 +1,17 @@
 import random
 
 def playAgain():
-    answer = input("Would you like to play again? Type Y for yes or N for no.")
-    if answer == "Y":
-        return 1
-    elif answer == "N":
-        print("Thanks for playing!")
-        return 0
-    else:
-        print("Error: your input was invalid.\n")
-        playAgain()
+    while True:
+        answer = input("\nWould you like to play again? Type Y for yes or N for no. ")
+        if answer == "Y":
+            return 1
+            break
+        elif answer == "N":
+            print("Thanks for playing!")
+            return 0
+            break
+        else:
+            print("Error: Your response is invalid.")
 
 def playGame():
     randomNumber = random.randint(1, 50)
@@ -17,23 +19,27 @@ def playGame():
     print("Guess a number between 1 and 50")
     i = 1
     while(numberOfGuesses < 11):
-        guess = int(input("Enter the number that you think is correct: "))
+        guess = int(input("\nEnter the number that you think is correct: "))
+        numberOfGuesses += 1;
         if (guess == randomNumber):
             print("Congratulations, you guessed the correct number", randomNumber, "in", numberOfGuesses, "guesses!")
-            numberOfGuesses = 0
             response = playAgain()
-            if response == 0:
+            if response == 1:
+                numberOfGuesses = 0
+                randomNumber = random.randint(1, 50)
+            elif response == 0:
                 break
         elif(guess < randomNumber):
             print("Your guess of", guess, "was too low")
         elif (guess > randomNumber):
             print("Your guess of", guess, "was too high")
-        numberOfGuesses += 1;
         if (numberOfGuesses == 10):
             print("You did not guess the right number. The correct number was", randomNumber)
-            numberOfGuesses = 0
             response = playAgain()
-            if response == 0:
+            if response == 1:
+                numberOfGuesses = 0
+                randomNumber = random.randint(1, 50)
+            elif response == 0:
                 break
 
 playGame()
