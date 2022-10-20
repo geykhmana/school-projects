2b5e85ac6ac30d1c005ef7d84905e64c7a220ac1
Alan Geykhman <alangeykhman@Alans-MacBook-Pro.local>
2022-10-20 13:18:24 -0400

    Added comments to project1

--- python/project1.py
+++ python/project1.py
@@ -14,31 +14,31 @@ def playAgain():
             print("Error: Your response is invalid.")
 
 def playGame():
-    randomNumber = random.randint(1, 50)
-    numberOfGuesses = 0
+    randomNumber = random.randint(1, 50)    #Setting random number
+    numberOfGuesses = 0                     #Setting number of guesses to 0 initially
     print("Guess a number between 1 and 50")
     while True:
         guess = int(input("\nEnter the number that you think is correct: "))
-        numberOfGuesses += 1;
-        if guess == randomNumber:
+        numberOfGuesses += 1; #Right after input is gathered, guess count increases by 1
+        if guess == randomNumber: #Correct guess case
             print("Congratulations, you guessed the correct number", randomNumber, "in", numberOfGuesses, "guesses!")
-            response = playAgain()
+            response = playAgain() #Asks if user wants to play again
             if response == 1:
                 numberOfGuesses = 0
-                randomNumber = random.randint(1, 50)
+                randomNumber = random.randint(1, 50) #Variables are reset, program automatically restarts due to while True loop
             elif response == 0:
-                break
+                break #Loop breaks, program ends
         elif guess < randomNumber:
-            print("Your guess of", guess, "was too low")
+            print("Your guess of", guess, "was too low") #Guess is too low case
         elif guess > randomNumber:
-            print("Your guess of", guess, "was too high")
+            print("Your guess of", guess, "was too high") #Guess is too high case
         if numberOfGuesses == 10:
             print("You did not guess the right number. The correct number was", randomNumber)
-            response = playAgain()
+            response = playAgain() #Asks if user wants to play again
             if response == 1:
                 numberOfGuesses = 0
-                randomNumber = random.randint(1, 50)
+                randomNumber = random.randint(1, 50) #Variables are reset, program automatically restarts due to while True loop
             elif response == 0:
-                break
+                break #Loop breaks, program ends
 
 playGame()
