303136e657015ae0cffdc6c5d991064216abfc93
Alan Geykhman <alangeykhman@Alans-MacBook-Pro.local>
2023-01-23 10:54:15 -0500

    Added semicolon

--- java/src/lab1/HelloWorld.java
+++ java/src/lab1/HelloWorld.java
@@ -2,6 +2,6 @@ package lab1;
 
 public class HelloWorld {
     public static void main(String[] arg) {
-        System.out.println("Hello world")
+        System.out.println("Hello world");
     }
 }
