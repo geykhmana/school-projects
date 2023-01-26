8d8251a60ebe955dd8baffc81229bef9698cd708
Alan Geykhman <alangeykhman@Alans-MacBook-Pro.local>
2023-01-26 09:58:24 -0500

    CSYes edits

--- java/src/lab1/lab1/CSYes.java
+++ java/src/lab1/lab1/CSYes.java
@@ -1,4 +1,13 @@
+// ************************************************************
+// File Name: CSYes.java
+//
+// Purpose: Print two messages
+// ************************************************************
 package lab1.lab1;
 
 public class CSYes {
+    public static void main(String[] args) {
+        System.out.println("Computer Science, Yes!!!!");
+        System.out.println("=========================");
+    }
 }
