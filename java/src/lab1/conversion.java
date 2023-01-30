cbd558d85b5f1c4bd76e1189682872214ff0ad6a
Alan Geykhman <alangeykhman@Alans-MacBook-Pro.local>
2023-01-30 10:44:52 -0500

    Changes to conversion.java

--- java/src/lab1/conversion.java
+++ java/src/lab1/conversion.java
@@ -3,6 +3,14 @@ package lab1;
 public class conversion {
     public static void main(String[] args) {
         int keys = 88;
-
+        byte age = 100;
+        long population = 12345678912L;
+        float price = 8.0f;
+        double balance = 8;
+        char letter = "A";
+        boolean isEligible = true;
+        String name = "Java";
+        System.out.println(balance);
+        System.out.println(keys);
     }
 }
