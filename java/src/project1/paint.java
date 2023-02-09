bddc71da344b6dc480d3f8adf2356a88353cc365
Alan Geykhman <alangeykhman@Alans-MacBook-Pro.local>
2023-02-09 10:13:18 -0500

    Adding project1 and paint.java

---
+++ java/src/project1/paint.java
@@ -0,0 +1,22 @@
+package project1;
+
+import java.util.Scanner;
+public class paint {
+    public static void main(String[] args) {
+        final int COVERAGE = 350; //paint covers 350 sq ft/gal
+        int length;
+        int width;
+        int height; //declare integers length, width, and height;
+//declare double totalSqFt;
+//declare double paintNeeded;
+//declare and initialize Scanner object
+//Prompt for and read in the length of the room
+//Prompt for and read in the width of the room
+//Prompt for and read in the height of the room
+//Compute the total square feet to be painted--think
+// about the dimensions of each wall
+//Compute the amount of paint needed
+//Print the length, width, and height of the room and the
+// number of gallons of paint needed.
+    }
+}
