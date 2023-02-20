f7a00a8ed655dfe7a94fee07a3df53dfa85a286f
geykhmana <alangeykhman@gmail.com>
2023-02-20 15:30:54 -0500

    Changes to Deli.java

--- java/src/project1/Deli.java
+++ java/src/project1/Deli.java
@@ -8,6 +8,9 @@ package project1;
 // nicely formatted, for the item.
 //
 // ************************************************************
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
 public class Deli {
     // ---------------------------------------------------
@@ -22,13 +25,11 @@ public class Deli {
         double weight; // weight in pounds
         double totalPrice; // total price for the item
         Scanner scan = new Scanner(System.in);
-        // Declare money as a NumberFormat object and use the
-        // getCurrencyInstance method to assign it a value
+        NumberFormat money = NumberFormat.getCurrencyInstance(Locale.US); // Declare money as a NumberFormat object and use the getCurrencyInstance method to assign it a value
         // Declare fmt as a DecimalFormat object and instantiate it to
-        // format numbers with at least one digit to the left of the
-        // decimal and the fractional part rounded to two digits.
+        DecimalFormat fmt = new DecimalFormat("#.00"); // format numbers with at least one digit to the left of the decimal and the fractional part rounded to two digits.
         // prompt the user and read in each input
-        System. out. println ("Welcome to the CS Deli! ! \n ");
+        System.out.println ("Welcome to the CS Deli!\n");
         System.out.print ("Enter the price per pound of your item: ");
         pricePerPound = scan.nextDouble();
         System.out.print ("Enter the weight (ounces): ");
@@ -36,7 +37,7 @@ public class Deli {
         // Convert ounces to pounds and compute the total price
         weight = weightOunces / OUNCES_PER_POUND;
         totalPrice = pricePerPound * weight;
-        // Print the label using the formatting objects fmt
+        System.out.println("$" + fmt.format(totalPrice)); // Print the label using the formatting objects fmt
         // for the weight in pounds and money for the prices
     }
 }
