b90d42f19ba345ca6eb476ebe8ad6bbd3e7c7466
geykhmana <alangeykhman@gmail.com>
2023-02-27 11:05:30 -0500

    Finished part of Cards.java

--- java/src/project1/Cards.java
+++ java/src/project1/Cards.java
@@ -1,19 +1,26 @@
 package project1;
 
 public class Cards {
-    enum Rank {
-        ace,
-        two,
-        three,
-        four,
-        five,
-        six,
-        seven,
-        eight,
-        nine,
-        ten,
-        jack,
-        queen,
-        king,
+    public static void main(String[] args) {
+        enum Rank {
+            ace,
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven,
+            eight,
+            nine,
+            ten,
+            jack,
+            queen,
+            king,
+        }
+        Rank highCard = Rank.ace;
+        Rank faceCard = Rank.king;
+        Rank card1 = Rank.seven;
+        Rank card2 = Rank.three;
+        System.out.println("A blackjack hand: " + highCard + " and " + faceCard);
     }
 }
