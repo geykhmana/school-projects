406e067fb2cfe6104c27e921c40020faf5b807d8
geykhmana <alangeykhman@gmail.com>
2023-02-27 11:11:15 -0500

    Finished Cards.java

--- java/src/project1/Cards.java
+++ java/src/project1/Cards.java
@@ -22,5 +22,9 @@ public class Cards {
         Rank card1 = Rank.seven;
         Rank card2 = Rank.three;
         System.out.println("A blackjack hand: " + highCard + " and " + faceCard);
+        int card1Val = card1.ordinal() + 1;
+        int card2Val = card2.ordinal() + 1;
+        System.out.println("A two card hand: " + card1 + " and " + card2);
+        System.out.println("Hand value: " + (card1Val+card2Val));
     }
 }
