2dbf7a63303e02511146a1afab7b1fce7bc63510
geykhmana <alangeykhman@gmail.com>
2023-02-27 10:30:41 -0500

    Created cards.java

---
+++ java/src/project1/cards.java
@@ -0,0 +1,19 @@
+package project1;
+
+public class cards {
+    enum Rank {
+        ace,
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine,
+        ten,
+        jack,
+        queen,
+        king,
+    }
+}
