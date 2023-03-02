12d919a290c81d2e95c9485990ec7b58f10b7716
geykhmana <alangeykhman@gmail.com>
2023-03-02 09:44:35 -0500

    Added Student.java

---
+++ java/src/project1/Student.java
@@ -0,0 +1,39 @@
+package project1;
+// ************************************************************
+// Student.java
+//
+// Define a student class that stores name, score on test 1, and
+// score on test 2. Methods prompt for and read in grades,
+// compute the average, and return a string containing student’s info.
+// ************************************************************
+import java.util.Scanner;
+public class Student {
+    //declare instance data
+    // ---------------------------------------------
+    //constructor
+    // ---------------------------------------------
+    public Student(String studentName) {
+    //add body of constructor
+    }
+        // ---------------------------------------------
+    //inputGrades: prompt for and read in student’s grades for test1 and test2.
+    //Use name in prompts, e.g., "Enter’s Joe’s score for test1".
+    // ---------------------------------------------
+        public void inputGrades() {
+    //add body of inputGrades
+        }
+        // ---------------------------------------------
+    //getAverage: compute and return the student’s test average
+    // ---------------------------------------------
+    //add header for getAverage
+        {
+    //add body of getAverage
+        }
+        // ---------------------------------------------
+    //getName: print the student’s name
+    // ---------------------------------------------
+    //add header for printName
+        {
+    //add body of printName
+        }
+}
