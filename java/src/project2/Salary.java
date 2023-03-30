dc19eafbe721d66083358090587c5e77baed5d94
geykhmana <alangeykhman@gmail.com>
2023-03-29 23:34:59 -0400

    Completed task 1 for Salary.java

--- java/src/project2/Salary.java
+++ java/src/project2/Salary.java
@@ -4,11 +4,12 @@ package project2;
 // Computes the raise and new salary for an employee
 // ************************************************************
 import java.util.Scanner;
+import java.text.NumberFormat;
 public class Salary {
     public static void main (String[] args) {
         double currentSalary; // current annual salary
-        double rating; // performance rating
-        double raise; // dollar amount of the raise
+        int rating; // performance rating
+        double raise = 0; // dollar amount of the raise
         double newSalary; // new salary for the employee
 
         Scanner scan = new Scanner(System.in);
@@ -16,8 +17,17 @@ public class Salary {
         // Get the current salary and performance rating
         System.out.print("Enter the current salary: ");
         currentSalary = scan.nextDouble();
+        System.out.println("Enter the employee's rating: ");
+        rating = scan.nextInt();
 
         // Compute the raise -- Use if ... else ...
+        if (rating == 1) {
+            raise = currentSalary * 0.06;
+        } else if (rating == 2) {
+            raise = currentSalary * 0.04;
+        } else if (rating == 3) {
+            raise = currentSalary * 0.015;
+        }
         newSalary = currentSalary + raise;
 
         // Print the results
