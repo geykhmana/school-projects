8a0ba206bf0ae5eaf507bf984a28f5327144d7b3
geykhmana <alangeykhman@gmail.com>
2023-03-30 10:20:47 -0400

    Completed task 2 for Factorial.java

--- java/src/project2/Factorial.java
+++ java/src/project2/Factorial.java
@@ -6,16 +6,23 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         int num;
+        int printnum = 0;
         int answer = 1;
 
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter a nonnegative integer: ");
         num = scan.nextInt();
-        int printnum = num;
+        if (num < 0) {
+            while (num < 0) {
+                System.out.println("Please enter a nonnegative integer: ");
+                num = scan.nextInt();
+            }
+        }
         if (num == 0) {
             answer = 1;
         } else {
+            printnum = num;
             while (num > 0) {
                 answer *= num;
                 num--;
