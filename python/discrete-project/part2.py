493ee2549d61af0346add6932fb7959074c340d3
geykhmana <alangeykhman@gmail.com>
2023-05-01 11:13:26 -0400

    Added to part2.py

--- python/discrete-project/part2.py
+++ python/discrete-project/part2.py
@@ -1,1 +1,10 @@
-choice = input("Would you like to convert a decimal number to binary or a binary number to decimal?\n(Type dtb for decimal to binary or btd for binary to decimal: ")
+while True:
+    choice = input("Would you like to convert a decimal number to binary or a binary number to decimal?\n(Type dtb for decimal to binary, btd for binary to decimal, or \"quit\" if you would like to quit the program: ")
+    if choice == "quit":
+        break
+    elif choice == "dtb":
+        dtb()
+    elif choice == "btd":
+        btd()
+    else:
+        print("Sorry, I could not understand your input.\n")
