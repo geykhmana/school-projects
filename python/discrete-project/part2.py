c2c61d8169bd73d9cf25ae69ac6417d4cf05e03b
geykhmana <alangeykhman@gmail.com>
2023-05-01 15:27:33 -0400

    1st version of completed btd() function in part2.py

--- python/discrete-project/part2.py
+++ python/discrete-project/part2.py
@@ -1,5 +1,7 @@
+import math
+
 def dtb():
-    dec = int(input("Enter the decimal integer you would like to convert: "))
+    dec = int(input("\nEnter the decimal integer you would like to convert: "))
     bin = ""
     if dec == 0:
         return 0
@@ -10,15 +12,23 @@ def dtb():
         bin = bin[::-1]
         return bin
 def btd():
-    bin = int(input("Enter the binary number you would like to convert: "))
+    bin = int(input("\nEnter the binary number you would like to convert: "))
+    dec = 0
+    binstr = str(bin)
+    i = 0
+    for x in binstr[::-1]:
+        if x == "1":
+            dec += 2**i
+        i += 1
+    return dec
 
 while True:
-    choice = input("Would you like to convert a decimal number to binary or a binary number to decimal?\n(Type dtb for decimal to binary, btd for binary to decimal, or \"quit\" if you would like to quit the program: ")
+    choice = input("\nWould you like to convert a decimal number to binary or a binary number to decimal?\n(Type dtb for decimal to binary, btd for binary to decimal, or \"quit\" if you would like to quit the program: ")
     if choice == "quit":
         break
     elif choice == "dtb":
         print(dtb())
     elif choice == "btd":
-        btd()
+        print(btd())
     else:
         print("Sorry, I could not understand your input.\n")
