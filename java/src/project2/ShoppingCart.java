e2e4cace79a146883ae7954ba0e1f97501a4ca6c
geykhmana <alangeykhman@gmail.com>
2023-05-05 14:14:07 -0400

    Overhauled ShoppingCart.java

--- java/src/project2/ShoppingCart.java
+++ java/src/project2/ShoppingCart.java
@@ -7,37 +7,53 @@ public class ShoppingCart {
     private int itemCount; // total number of items in the cart
     private double totalPrice; // total price of items in the cart
     private int capacity; // current cart capacity
-    // ---------------------------------------------------------
+    private Item cart[];
+
     // Creates an empty shopping cart with a capacity of 5 items.
-    // ---------------------------------------------------------
     public ShoppingCart() {
         capacity = 5;
+        //create an array of Item type of capacity 5
+        cart = new Item[capacity];
+        //set itemCount and totalPrice to zero
         itemCount = 0;
         totalPrice = 0.0;
     }
-    // -----------------------------------------------------
-    // Adds an item to the shopping cart .
-    // -----------------------------------------------------
+
+    // Adds an item to the shopping cart.
     public void addToCart(String itemName, double price, int quantity) {
+        //instatiate a Item class object with three input
+        //parameters called itemName, price and quantity
+        if (itemCount < capacity) {
+            cart[itemCount] = new Item(itemName, price, quantity);
+            //incremnt the itemCount by one
+            itemCount++;
+        } else {
+            //increaseSize();
+            cart[itemCount] = new Item(itemName, price, quantity);
+            //increment the itemCount by one
+            itemCount++;
+        }
     }
-    // -----------------------------------------------------
-    // Returns the contents of the cart together with summary information.
-    // -----------------------------------------------------
+
+    // Returns the contents of the cart together with
+    // summary information.
     public String toString() {
         NumberFormat fmt = NumberFormat.getCurrencyInstance();
         String contents = "\nShopping Cart\n";
-        contents += "\nItem\t\tUnit Price\tQuantity\tTotal\n";
-        for (int i = 0;i < itemCount;i++) {
+        contents += "\nItem\tUnit Price\tQuantity\tTotal\n";
+
+        for (int i = 0; i < itemCount; i++) {
             contents += cart[i].toString() + "\n";
         }
-        contents += "\nTotal Price: " + fmt.format(totalPrice);
-        contents += "\n";
         return contents;
     }
 
-    // -----------------------------------------------------
-    // Increases the capacity of the shopping cart by 3
-    // -----------------------------------------------------
-    private void increaseSize() {
+    //The method getTotalCost that calculates the total price
+//of all items in the cart and returns totalPrice
+    public double getTotalCost() {
+        for (int count = 0; count < itemCount; count++) {
+            totalPrice += cart[count].getPrice() * cart[count].getQuantity();
+        }
+        return totalPrice;
     }
 }
