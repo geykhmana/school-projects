2d294fdafda72145a0868dfd9ded23a8ebccf21b
geykhmana <alangeykhman@gmail.com>
2023-09-11 13:07:05 -0400

    Working further on part 3 of lab1

--- java/java2project1/src/lab1/NumCounter.java
+++ java/java2project1/src/lab1/NumCounter.java
@@ -1,8 +1,21 @@
 package lab1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class NumCounter {
-    ArrayList<Integer> intList = new ArrayList<Integer>();
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        Scanner getNumInput = new Scanner(System.in);
+        int i = 0;
+
+        while (i < 7) {
+            System.out.println("Enter an integer from 0 to 50 (inclusive): ");
+            int newInt = getNumInput.nextInt();
+            intList.add(newInt);
+            i++;
+        }
+
+        System.out.println(intList);
+    }
 }
