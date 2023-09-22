7ff24c32a5d4a20afeb78bfe363b97b0ae4a5c9f
geykhmana <alangeykhman@gmail.com>
2023-09-22 10:52:54 -0400

    Formatting changes

--- java/java2project1/src/sorting/Search.java
+++ java/java2project1/src/sorting/Search.java
@@ -1,47 +1,48 @@
 package sorting;
 
 public class Search {
-	public static Comparable linearSearch (Comparable[] list, Comparable target)
-	{
+	public static Comparable linearSearch (Comparable[] list, Comparable target) {
 		int index = 0;
 		boolean found = false;
 		
-		while (!found && index < list.length)
-		{
-			if (list[index].equals(target)) // Comparing two contacts
+		while (!found && index < list.length) {
+			if (list[index].equals(target)) { // Comparing two contacts
 				found = true;
-			else
+			} else {
 				index++;
+			}
 		}
 		
-		if (found)
+		if (found) {
 			return list[index];
-		else
+		} else {
 			return null;
+		}
 	}
 	
-	public static Comparable binarySearch (Comparable[] list,
-            Comparable target)
-	{
+	public static Comparable binarySearch (Comparable[] list, Comparable target) {
+
 		int min=0, max=list.length, mid=0;
 		boolean found = false;
 		
-		while (!found && min <= max)
-		{
+		while (!found && min <= max) {
 			mid = (min+max) / 2;
-			if (list[mid].equals(target))
+			if (list[mid].equals(target)) {
 				found = true;
-			else
-				if (target.compareTo(list[mid]) < 0)
-					max = mid-1;
-				else
-					min = mid+1;
+			} else {
+				if (target.compareTo(list[mid]) < 0) {
+					max = mid - 1;
+				} else {
+					min = mid + 1;
+				}
+			}
 		}
 
-	    if (found)
-	    	return list[mid];
-	    else
-	    	return null;
+	    if (found) {
+			return list[mid];
+		} else {
+			return null;
+		}
 	   }
 
 
