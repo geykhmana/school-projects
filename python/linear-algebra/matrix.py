0391303b7b912c5d4fab1b7c3d361201abb2aa5f
geykhmana <alangeykhman@gmail.com>
2023-09-22 12:17:16 -0400

    Added linear algebra example

--- python/linear-algebra/matrix.py
+++ python/linear-algebra/matrix.py
@@ -1,3 +1,6 @@
 import numpy as np
 
-A = np.array([[1.0, 1, 1], [1, 2, 4], [1, 3, 9]])
+A = np.array([[8.0, 3, -2], [-4, 7, 5], [3, 4, -12]])
+b = np.array([[9.0], [15], [35]])
+Ans1 = np.linalg.solve(A, b)
+print(Ans1)
