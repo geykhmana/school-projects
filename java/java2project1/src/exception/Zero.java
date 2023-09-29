4e403a11a6b51822e7b3a1da7a2bd0eb554e7d50
geykhmana <alangeykhman@gmail.com>
2023-09-29 10:04:51 -0400

    Added Zero.java

---
+++ java/java2project1/src/exception/Zero.java
@@ -0,0 +1,18 @@
+package exception;//********************************************************************
+//  Zero.java       Author: Lewis/Loftus
+//
+//  Demonstrates an uncaught exception.
+//********************************************************************
+
+public class Zero {
+   //-----------------------------------------------------------------
+   //  Deliberately divides by zero to produce an exception.
+   //-----------------------------------------------------------------
+   public static void main (String[] args) {
+      int numerator = 10;
+      int denominator = 0;
+      System.out.println (numerator / denominator);
+
+      System.out.println ("This text will not be printed.");
+   }
+}
