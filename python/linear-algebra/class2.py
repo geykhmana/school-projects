abff67346c60a3a7e44ab938d819c5ada805db44
geykhmana <alangeykhman@gmail.com>
2023-10-10 11:43:06 -0400

    Added more

--- python/linear-algebra/class2.py
+++ python/linear-algebra/class2.py
@@ -2,7 +2,14 @@ import numpy as np
 
 A = np.array([[2.2, 0.4, 9], [-1.2, 3.5, 7]])
 B = np.array([[1.5, 19], [-1, 3.5], [4.5, 8]])
+I3 = np.eye(3, 3)
+D = np.array([[1, 0, 3], [-2, 4, 1], [10, 5, -2.7]])
 
 print(A @ B)
 print(np.matmul(A, B))
-print(A+1)
+print(A + 1)
+print(I3)
+print(A.T)
+print(B.T)
+print(np.linalg.inv(D))
+print(np.multiply(A, A))
