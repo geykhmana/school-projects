b236fdd82470422d698336e705714e5cb294e955
geykhmana <alangeykhman@gmail.com>
2023-10-10 11:35:09 -0400

    Added more to class2.py

--- python/linear-algebra/class2.py
+++ python/linear-algebra/class2.py
@@ -1,4 +1,8 @@
 import numpy as np
-A=np.array([[2.2,0.4,9],[-1.2,3.5,7]])
-B=np.array([[1.5,19],[-1,3.5],[4.5,8]])
-print(A@B)
+
+A = np.array([[2.2, 0.4, 9], [-1.2, 3.5, 7]])
+B = np.array([[1.5, 19], [-1, 3.5], [4.5, 8]])
+
+print(A @ B)
+print(np.matmul(A, B))
+print(A+1)
