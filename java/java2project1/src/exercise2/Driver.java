5fcb819df5598971d19c843a77228837b18986bf
geykhmana <alangeykhman@gmail.com>
2023-10-10 10:42:56 -0400

    Added prompts for user to enter strings in Driver class

--- java/java2project1/src/exercise2/Driver.java
+++ java/java2project1/src/exercise2/Driver.java
@@ -6,8 +6,11 @@ public class Driver {
     public static void main(String[] args) {
         Scanner getInput = new Scanner(System.in);
 
+        System.out.println("Please enter the name of a legislative bill: ");
         String bill = getInput.nextLine();
+        System.out.println("Please enter the name of a sports team: ");
         String team = getInput.nextLine();
+        System.out.println("Please enter the name of a movie: ");
         String movie = getInput.nextLine();
 
         SpeakerOfTheHouse alan = new SpeakerOfTheHouse(); //Since there is no US speaker of the house, I had to step in.
