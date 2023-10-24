1a090881ee0e1c6c1fdd931e050ace6c368b5978
geykhmana <alangeykhman@gmail.com>
2023-10-24 11:37:43 -0400

    Reformatting

--- python/linear-algebra/plot.py
+++ python/linear-algebra/plot.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import plot, ion, show
+
 matplotlib.use('tkagg')
 
 ion()
-x=np.linspace(0, 2*math.pi, 200)
+x = np.linspace(0, 2 * math.pi, 200)
 plt.plot(x, np.sin(x))
 plt.xlabel("x")
 plt.ylabel("sin(x)")
