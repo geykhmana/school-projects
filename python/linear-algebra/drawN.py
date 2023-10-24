3c1c3fcb8977f8cdbdff39100d6d690b45f5e368
geykhmana <alangeykhman@gmail.com>
2023-10-24 12:25:20 -0400

    Plot generated by drawN.py, looks wrong but it's the exact code the prof put on the board

--- python/linear-algebra/drawN.py
+++ python/linear-algebra/drawN.py
@@ -6,12 +6,13 @@ from matplotlib.pyplot import plot, ion, show
 
 matplotlib.use("tkagg")
 
+ion()
 N = np.matrix([[0, 0.5, 0.5, 6, 6, 5.5, 5.5, 0],
                [0, 0, 6.42, 0, 8, 8, 1.58, 8]])
-adj = np.matrix([0, 1, 0, 0, 0, 0, 0, 1], [1, 0, 1, 0, 0, 0, 0, 0],
+adj = np.matrix([[0, 1, 0, 0, 0, 0, 0, 1], [1, 0, 1, 0, 0, 0, 0, 0],
                 [0, 1, 0, 1, 0, 0, 0, 0], [0, 0, 1, 0, 1, 0, 0, 0],
                 [0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 1, 0],
-                [0, 0, 0, 0, 0, 1, 0, 1], [1, 0, 0, 0, 0, 0, 1, 0])
+                [0, 0, 0, 0, 0, 1, 0, 1], [1, 0, 0, 0, 0, 0, 1, 0]])
 
 f1, ax1 = plt.subplots(1)
 for i in range(8):
@@ -19,3 +20,5 @@ for i in range(8):
         if adj[i, j] == 1:
             ax1.plot([N[0, i], N[0, j], N[1, i], N[1, j], 'b'])
 ax1.axis("off")
+show()
+plt.savefig("n-plot.png")
