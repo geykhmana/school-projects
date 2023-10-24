ff26705278a935e21fd9fcdeefe4c1602509ba9c
geykhmana <alangeykhman@gmail.com>
2023-10-24 12:34:55 -0400

    The transformed N looks correct...

--- python/linear-algebra/drawN.py
+++ python/linear-algebra/drawN.py
@@ -10,15 +10,29 @@ ion()
 N = np.matrix([[0, 0.5, 0.5, 6, 6, 5.5, 5.5, 0],
                [0, 0, 6.42, 0, 8, 8, 1.58, 8]])
 adj = np.matrix([[0, 1, 0, 0, 0, 0, 0, 1], [1, 0, 1, 0, 0, 0, 0, 0],
-                [0, 1, 0, 1, 0, 0, 0, 0], [0, 0, 1, 0, 1, 0, 0, 0],
-                [0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 1, 0],
-                [0, 0, 0, 0, 0, 1, 0, 1], [1, 0, 0, 0, 0, 0, 1, 0]])
+                 [0, 1, 0, 1, 0, 0, 0, 0], [0, 0, 1, 0, 1, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 1, 0],
+                 [0, 0, 0, 0, 0, 1, 0, 1], [1, 0, 0, 0, 0, 0, 1, 0]])
 
 f1, ax1 = plt.subplots(1)
 for i in range(8):
     for j in range(i + 1):
         if adj[i, j] == 1:
             ax1.plot([N[0, i], N[0, j], N[1, i], N[1, j], 'b'])
+
 ax1.axis("off")
 show()
 plt.savefig("n-plot.png")
+
+A = np.matrix([[1, 0.25], [0, 1]])  # Shear
+SN = A * N
+f2, ax2 = plt.subplots(1)
+
+for i in range(8):
+    for j in range(i + 1):
+        if adj[i, j] == 1:
+            ax2.plot([SN[0, i], SN[0, j]], [SN[1, i], SN[1, j]], 'b')
+
+ax1.axis("off")
+show()
+plt.savefig("n-plot-transformed.png")
