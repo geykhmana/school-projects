55713ae00e8ac6afddb98ccd606f327f2f65fbaa
geykhmana <alangeykhman@gmail.com>
2023-10-24 11:36:43 -0400

    Added plot.py

---
+++ python/linear-algebra/plot.py
@@ -0,0 +1,15 @@
+import math
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import plot, ion, show
+matplotlib.use('tkagg')
+
+ion()
+x=np.linspace(0, 2*math.pi, 200)
+plt.plot(x, np.sin(x))
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+plt.title("sin(x) vs x")
+show()
+plt.savefig('sine.png')
