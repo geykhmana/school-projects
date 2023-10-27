3c2b7a764a3db1c3de5918a889fb67e60e409448
geykhmana <alangeykhman@gmail.com>
2023-10-27 18:33:59 -0400

    Modified width of k, added title

--- python/linear-algebra/draw_k.py
+++ python/linear-algebra/draw_k.py
@@ -7,7 +7,7 @@ from matplotlib.pyplot import plot, ion, show
 matplotlib.use("tkagg")
 
 ion()
-k = np.matrix([[0, 0, 1, 1, 5, 6, 1.555555, 6, 5, 1, 1],
+k = np.matrix([[0, 0, 0.7, 0.7, 4, 5, 1.4, 5, 4, 0.7, 0.7],
                [0, 10, 10, 4.25, 6.5, 6.5, 4, 0, 0, 3.6, 0]])
 adj = np.matrix([[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1], [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
                  [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
@@ -22,8 +22,9 @@ for i in range(11):
         if adj[i, j] == 1:
             ax1.plot([k[0, i], k[0, j]], [k[1, i], k[1, j]], 'b')
 
-plt.xlim(0, 14)
+plt.xlim(0, 12)
 plt.ylim(0, 10)
 ax1.axis("off")
+ax1.set_title("Regular k")
 show()
 plt.savefig("k-plot.png")
