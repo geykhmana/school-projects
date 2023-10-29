49c05be7fd82100a33008f2b794e95ec038ad833
geykhmana <alangeykhman@gmail.com>
2023-10-29 15:39:09 -0400

    Added code for the regular and reversed side-by-side plots, image generated

--- python/linear-algebra/draw_k.py
+++ python/linear-algebra/draw_k.py
@@ -43,3 +43,19 @@ for i in range(11):
 ax2.axis("off")
 ax2.set_title("Rotated k")
 plt.savefig("rotated-k-plot.png")
+
+T = np.matrix([[-1, 0],
+               [0, 1]])
+kT = T * k
+
+f5, ax5 = plt.subplots(1, 2)
+for i in range(11):
+    for j in range(i + 1):
+        if adj[i, j] == 1:
+            ax5[0].plot([k[0, i], k[0, j]], [k[1, i], k[1, j]], "r")
+            ax5[1].plot([kT[0, i], kT[0, j]], [kT[1, i], kT[1, j]], "b")
+ax5[0].axis("off")
+ax5[1].axis("off")
+ax5[0].set_title("Regular k")
+ax5[1].set_title("Reversed k")
+plt.savefig("regular-and-reversed-k-plots.png")
