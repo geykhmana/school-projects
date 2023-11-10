5fdb82b2d5288ee5bee7be0028c9eaee1637a673
geykhmana <alangeykhman@gmail.com>
2023-11-10 10:39:42 -0500

    Added Fahrenheit files

---
+++ java/javafxdemo/src/main/java/com/example/javafxdemo/FahrenheitConverter.java
@@ -0,0 +1,23 @@
+package com.example.javafxdemo;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+//************************************************************************
+//� FahrenheitConverter.java � � � Author: Lewis/Loftus
+//
+//� Demonstrates the use of a TextField and a GridPane.
+//************************************************************************
+
+public class FahrenheitConverter extends Application {
+
+	public void start(Stage primaryStage) {
+		Scene scene = new Scene(new FahrenheitPane(), 300, 150);
+		primaryStage.setTitle("Fahrenheit Converter");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
