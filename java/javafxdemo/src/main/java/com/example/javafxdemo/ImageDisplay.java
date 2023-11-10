458a690de425aa600a71d46b57efec2187f51964
geykhmana <alangeykhman@gmail.com>
2023-11-10 10:03:03 -0500

    Added ImageDisplay.java

---
+++ java/javafxdemo/src/main/java/com/example/javafxdemo/ImageDisplay.java
@@ -0,0 +1,27 @@
+package com.example.javafxdemo;
+
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ImageDisplay extends Application{
+	public void start(Stage primaryStage) {
+		Image img = new Image("E://Projects//Java//GUIDesign//index.png");
+		ImageView imgView=new ImageView(img);
+		
+		StackPane pane=new StackPane(imgView);
+		pane.setStyle("-fx-background-color: cornsilk");
+		
+		Scene scene = new Scene(pane,500,300);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
