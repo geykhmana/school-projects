1b0d59cd56ef46a551dc9810fcb3b014c2a50646
geykhmana <alangeykhman@gmail.com>
2023-11-16 11:01:26 -0500

    Renamed the two older images and added part 3

--- python/linear-algebra/computer_graphics.py
+++ python/linear-algebra/computer_graphics.py
@@ -27,16 +27,16 @@ P = np.matrix([[1, 0, -b / d, 0], [0, 1, -c / d, 0], [0, 0, 0, 0], [0, 0, -1 / d
 PD = P * D
 PD = PD / PD[3, :]
 
-f, ax = plt.subplots(1)
+f1, ax1 = plt.subplots(1)
 
 for i in range(16):
     for j in range(i + 1):
         if C[i, j] == 1:
-            ax.plot([PD[0, i], PD[0, j]], [PD[1, i], PD[1, j]], "g")
+            ax1.plot([PD[0, i], PD[0, j]], [PD[1, i], PD[1, j]], "g")
 
-ax.set_title("Center at " + str(center))
-ax.axis("off")
-plt.savefig("ToyotaQ1.png")
+ax1.set_title("Center at " + str(center))
+ax1.axis("off")
+plt.savefig("Toyota_-5_10_10.png")
 
 b, c, d = 0, 10, 25  # center
 center = (b, c, d)
@@ -44,14 +44,39 @@ P = np.matrix([[1, 0, -b / d, 0], [0, 1, -c / d, 0], [0, 0, 0, 0], [0, 0, -1 / d
 PD = P * D
 PD = PD / PD[3, :]
 
-f, ax = plt.subplots(1)
+f2, ax2 = plt.subplots(1)
 
 for i in range(16):
     for j in range(i + 1):
         if C[i, j] == 1:
-            ax.plot([PD[0, i], PD[0, j]], [PD[1, i], PD[1, j]], "g")
+            ax2.plot([PD[0, i], PD[0, j]], [PD[1, i], PD[1, j]], "g")
 
-ax.set_title("Center at " + str(center))
-ax.axis("off")
-plt.savefig("ToyotaQ2.png")
+ax2.set_title("Center at " + str(center))
+ax2.axis("off")
+plt.savefig("Toyota_0_10_25.png")
 
+phi = math.pi / 6
+R = np.matrix([[math.cos(phi), 0,  math.sin(phi), 0],
+               [0, 1, 0, 0],
+               [-math.sin(phi), 0, math.cos(phi), 0],
+               [0, 0, 0, 1]])
+RD = R * D
+b, c, d = 0, 10, 25  # center
+center = (b, c, d)
+P = np.matrix([[1, 0, -b / d, 0], [0, 1, -c / d, 0], [0, 0, 0, 0], [0, 0, -1 / d, 1]])
+PD = P * D
+PD = PD / PD[3, :]
+PRD = P * RD
+PRD = PRD / PRD[3, :]
+
+f3, ax3 = plt.subplots(1, 2)
+for i in range(16):
+    for j in range(i + 1):
+        if C[i, j] == 1:
+            ax3[0].plot([PD[0, i], PD[0, j]], [PD[1, i], PD[1, j]], "g")
+            ax3[1].plot([PRD[0, i], PRD[0, j]], [PRD[1, i], PRD[1, j]], "r")
+ax3[0].axis("off")
+ax3[1].axis("off")
+ax3[0].set_title("Center at " + str(center))
+ax3[1].set_title("Rotated 30° about the y axis")
+plt.savefig("Toyota_Normal_and_Rotated.png")
