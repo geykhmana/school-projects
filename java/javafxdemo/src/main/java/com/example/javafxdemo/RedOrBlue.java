ccc85e8a57670536cfca2404b9fe6064fdbde72a
geykhmana <alangeykhman@gmail.com>
2023-11-17 10:12:34 -0500

    yea

---
+++ java/javafxdemo/src/main/java/com/example/javafxdemo/RedOrBlue.java
@@ -0,0 +1,50 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+//************************************************************************
+//  RedOrBlue.java       Author: Lewis/Loftus
+//
+//  Demonstrates the use of one handler for multiple buttons.
+//************************************************************************
+
+public class RedOrBlue extends Application
+{
+	private Button redButton, blueButton;
+	private FlowPane pane;
+	
+	public void start(Stage primaryStage)
+	{
+		redButton = new Button("Red!");
+		redButton.setOnAction(this::processColorButton);
+		
+		blueButton = new Button("Blue!");
+		blueButton.setOnAction(this::processColorButton);
+		
+		pane = new FlowPane(redButton, blueButton);
+		pane.setAlignment(Pos.CENTER);
+		pane.setHgap(20);
+		pane.setStyle("-fx-background-color: white");
+		
+		Scene scene = new Scene(pane, 300, 100);
+		
+		primaryStage.setTitle("Red or Blue?");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public void processColorButton(ActionEvent event)
+	{
+		if (event.getSource() == redButton)
+			pane.setStyle("-fx-background-color: crimson");
+		else
+			pane.setStyle("-fx-background-color: deepskyblue");
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
