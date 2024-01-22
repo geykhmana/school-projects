224c5a8a261782a86369763e5ad33917e52aedc1
geykhmana <alangeykhman@gmail.com>
2024-01-22 16:11:47 -0500

    Added HW1 package and Sort.java

---
+++ java/csit212/src/main/java/HW1/Sort.java
@@ -0,0 +1,90 @@
+package HW1;
+
+import java.util.*;
+
+public class Sort {
+	public static int[] insertion_sort(int[] array) {
+		/*
+		 * fill in your program
+		 */
+		return array;
+	}
+
+	/*
+	 * n: the size of the output array
+	 * k: the maximum value in the array
+	 */
+	public static int[] generate_random_array(int n, int k) {
+		List<Integer> list;
+		int[] array;
+		Random rnd;
+		
+		rnd = new Random(System.currentTimeMillis());
+		
+		list = new ArrayList<Integer> ();
+		for (int i = 1; i <= n; i++) 
+			list.add(new Integer(rnd.nextInt(k+1)));
+		
+		Collections.shuffle(list, rnd);
+		
+		array = new int[n];
+		for (int i = 0; i < n; i++) 
+			array[i] = list.get(i).intValue();
+		
+		return array;
+	}
+	
+	/*
+	 * n: the size of the output array
+	 */
+	public static int[] generate_random_array(int n) {
+		List<Integer> list;
+		int[] array;
+		
+		list = new ArrayList<Integer> ();
+		for (int i = 1; i <= n; i++) 
+			list.add(new Integer(i));
+		
+		Collections.shuffle(list, new Random(System.currentTimeMillis()));
+		
+		array = new int[n];
+		for (int i = 0; i < n; i++) 
+			array[i] = list.get(i).intValue();
+		
+		return array;
+	}
+	
+	/*
+	 * Input: an integer array
+	 * Output: true if the array is acsendingly sorted, otherwise return false
+	 */
+	public static boolean check_sorted(int[] array) {
+		for (int i = 1; i < array.length; i++) {
+			if (array[i-1] > array[i])
+				return false;
+		}
+		return true;
+	}
+	
+	public static void print_array(int[] array) {
+		for (int i = 0; i < array.length; i++)
+			System.out.print(array[i] + ", ");
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Insertion sort starts ------------------");
+		for (int n = 100000; n <= 1000000; n=n+100000) {
+			int[] array = Sort.generate_random_array(n);
+			long t1 = System.currentTimeMillis();
+			array = Sort.insertion_sort(array);
+			long t2 = System.currentTimeMillis();
+			long t = t2 - t1;
+			boolean flag = Sort.check_sorted(array);
+			System.out.println(n + "," + t + "," + flag);
+		}
+		System.out.println("Insertion sort ends ------------------");
+	}
+
+}
