3bd7e589636cef5d93977b78db7479da30b29def
geykhmana <alangeykhman@gmail.com>
2024-02-09 13:02:38 -0500

    Added Sort4, trying to get Sort3 counting sort to work

--- java/csit212/src/main/java/sorting/Sort4.java
+++ java/csit212/src/main/java/sorting/Sort4.java
@@ -2,56 +2,43 @@ package sorting;
 
 import java.util.*;
 
-public class Sort3 {
+public class Sort4 {
+	
+	public static int left (int i) {
+		
+	}
+	
+	public static int right (int i) {
+		
+	}
+	
+	public static int parent (int i) {
+		
+	}
+	
+	public static int[] max_heapify (int[] array, int heap_size, int i) {
+			
+	}
+
+	
+	public static int[] build_heap (int[] array) {
+	
+	}
+	
+	public static int[] heap_sort (int[] array) {
+	
+	}
+	
 	public static int[] quick_sort (int[] array, int p, int r) {
-		int q;
-		if (p < r) {
-			q = partition(array, p, r);
-			quick_sort(array, p, q-1);
-			quick_sort(array, q+1, r);
-		}
-		return array;
+		
 	}
 	
 	public static int partition (int[] array, int p, int r) {
-		int x = array[r];
-		int i = p - 1;
-
-		for (int j = p; j < r; j++) {
-			if (array[j] <= x) {
-				i++;
-				int temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-		}
-
-		int temp2 = array[i + 1];
-		array[i + 1] = array[r];
-		array [r] = temp2;
-
-		return i + 1;
+		
 	}
 	
 	public static int[] counting_sort (int[] array, int k) {
-		int[] C = new int[k];
-		int[] B = new int[0];
-
-		for (int i = 0; i < k; i++) {
-			C[i] = 0;
-		}
-		for (int j = 1; j < array.length; j++) {
-			C[array[j]] += 1;
-		}
-		for (int i = 1; i < k; i++) {
-			C[i] += C[i-1];
-		}
-		for (int j = array.length; j > 1; j--) {
-			B[C[array[j]]] = array[j];
-			C[array[j]] -= 1;
-		}
-
-		return array;
+		
 	}
 	
 	
@@ -114,26 +101,38 @@ public class Sort3 {
 		// TODO Auto-generated method stub
 		int k = 10000;
 		
-		System.out.println("Quick sort starts ------------------");
+		System.out.println("Heap sort starts ------------------");
 		for (int n = 100000; n <= 1000000; n=n+100000) {
-			int[] array = Sort3.generate_random_array(n);
+			int[] array = Sort2.generate_random_array(n);
 			long t1 = System.currentTimeMillis();
-			array = Sort3.quick_sort(array, 0, n-1);
+			array = Sort4.heap_sort(array);
 			long t2 = System.currentTimeMillis();
 			long t = t2 - t1;
-			boolean flag = Sort3.check_sorted(array);
+			boolean flag = Sort2.check_sorted(array);
+			System.out.println(n + "," + t + "," + flag);
+		}
+		System.out.println("Heap sort ends ------------------");
+		
+		System.out.println("Quick sort starts ------------------");
+		for (int n = 100000; n <= 1000000; n=n+100000) {
+			int[] array = Sort2.generate_random_array(n);
+			long t1 = System.currentTimeMillis();
+			array = Sort4.quick_sort(array, 0, n-1);
+			long t2 = System.currentTimeMillis();
+			long t = t2 - t1;
+			boolean flag = Sort2.check_sorted(array);
 			System.out.println(n + "," + t + "," + flag);
 		}
 		System.out.println("Quick sort ends ------------------");
 		
 		System.out.println("Counting sort starts ------------------");
 		for (int n = 100000; n <= 1000000; n=n+100000) {
-			int[] array = Sort3.generate_random_array(n, k);
+			int[] array = Sort2.generate_random_array(n, k);
 			long t1 = System.currentTimeMillis();
-			array = Sort3.counting_sort(array, k);
+			array = Sort4.counting_sort(array, k);
 			long t2 = System.currentTimeMillis();
 			long t = t2 - t1;
-			boolean flag = Sort3.check_sorted(array);
+			boolean flag = Sort2.check_sorted(array);
 			System.out.println(n + "," + t + "," + flag);
 		}
 		System.out.println("Counting sort ends ------------------");
