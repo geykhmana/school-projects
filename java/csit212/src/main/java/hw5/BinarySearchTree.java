0f5763f2b4b4e11c9bb22efbdbd0f669a0d94c28
geykhmana <alangeykhman@gmail.com>
2024-02-27 12:56:00 -0500

    Added HW5 files

---
+++ java/csit212/src/main/java/hw5/BinarySearchTree.java
@@ -0,0 +1,87 @@
+package ds;
+
+public class BinarySearchTree {
+	public TreeNode root;
+	
+	public BinarySearchTree () {
+		root = null;
+	}
+	
+	public void inorder_tree_walk (TreeNode x) {
+		
+	}
+	
+	public TreeNode search (TreeNode x, int k) {
+		
+	}
+	
+	public TreeNode iterative_search (int k) {
+		
+	}
+	
+	public TreeNode minimum () {
+		
+	}
+	
+	public TreeNode maximum () {
+		
+	}
+	
+	public TreeNode successor (TreeNode x) {
+		
+	}
+	
+	public void insert (int k) {
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {15, 6, 18, 3, 7, 17, 20, 2, 4, 13, 9};
+		BinarySearchTree bst;
+		TreeNode n;
+		
+		bst = new BinarySearchTree();
+		for (int i = 0; i < array.length; i++)
+			bst.insert(array[i]);
+		
+		System.out.println("Inorder_tree_walk starts ------------------");
+		bst.inorder_tree_walk(bst.root);
+		System.out.println("Inorder_tree_walk ends ------------------");
+		System.out.print("\n\n");
+		
+		System.out.println("Search starts ------------------");
+		n = bst.search(bst.root, 13);
+		System.out.println("found: " + n.key);
+		System.out.println("Search ends ------------------");
+		System.out.print("\n\n");
+
+		System.out.println("Iterative search starts ------------------");
+		n = bst.iterative_search(4);
+		System.out.println("found: " + n.key);
+		System.out.println("Iterative search ends ------------------");
+		System.out.print("\n\n");
+		
+		System.out.println("Minimum starts ------------------");
+		n = bst.minimum();
+		System.out.println("Minimum key is " + n.key);
+		System.out.println("Minimum ends ------------------");
+		System.out.print("\n\n");
+		
+		System.out.println("Maximum starts ------------------");
+		n = bst.maximum();
+		System.out.println("Maximum key is " + n.key);
+		System.out.println("Maximum ends ------------------");
+		System.out.print("\n\n");
+
+		System.out.println("Successor starts ------------------");
+		n = bst.successor(bst.root.left.right.right);
+		System.out.println("Successor key is " + n.key);
+		System.out.println("Successor ends ------------------");
+		System.out.print("\n\n");
+	}
+
+}
