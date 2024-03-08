3ea1e172f3a8ce51d90e44c92faf2ae5ddb965a2
geykhmana <alangeykhman@gmail.com>
2024-03-08 10:59:27 -0500

    Changes to TreeNode methods

--- java/csit212/src/main/java/hw5/BinarySearchTree.java
+++ java/csit212/src/main/java/hw5/BinarySearchTree.java
@@ -8,27 +8,59 @@ public class BinarySearchTree {
 	}
 	
 	public void inorder_tree_walk (TreeNode x) {
-		
+		if (x != null) {
+			inorder_tree_walk(x.left);
+			System.out.println(x.key);
+			inorder_tree_walk(x.right);
+		}
 	}
 	
 	public TreeNode search (TreeNode x, int k) {
-		
+		if (x == null || k == x.key) {
+			return x;
+		}
+		if (k < x.key) {
+			return search(x.left, k);
+		} else {
+			return search(x.right, k);
+		}
 	}
 	
-	public TreeNode iterative_search (int k) {
-		
+	public TreeNode iterative_search (TreeNode x, int k) {
+		while (x != null && k != x.key) {
+			if (k < x.key) {
+				x = x.left;
+			} else {
+				x = x.right;
+			}
+		}
+		return x;
 	}
 	
-	public TreeNode minimum () {
-		
+	public TreeNode minimum (TreeNode x) {
+		while (x.left != null) {
+			x = x.left;
+		}
+		return x;
 	}
 	
-	public TreeNode maximum () {
-		
+	public TreeNode maximum (TreeNode x) {
+		while (x.right != null) {
+			x = x.right;
+		}
+		return x;
 	}
 	
 	public TreeNode successor (TreeNode x) {
-		
+		if (x.right != null) {
+			return minimum(x.right);
+		}
+		TreeNode y = x.p;
+		while (y != null && x == y.right) {
+			x = y;
+			y = y.p;
+		}
+		return y;
 	}
 	
 	public void insert (int k) {
