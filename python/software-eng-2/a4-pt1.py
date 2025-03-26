bbeaaa4e105dcc3b42fccec1799ec66bedbd57ec
geykhmana <alangeykhman@gmail.com>
2025-03-25 22:09:42 -0400

    Refactored assignment 4 file
    Renamed assignment4.py to a4-pt1.py, will be run in main file

---
+++ python/software-eng-2/a4-pt1.py
@@ -0,0 +1,15 @@
+def distance(x1, x2, y1, y2):
+    d = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+
+    return d
+
+if __name__ == "__main__":
+    x1 = int(input("Enter the x value for point 1: "))
+    y1 = int(input("Enter the y value for point 1: "))
+    print("\n")
+    x2 = int(input("Enter the x value for point 2: "))
+    y2 = int(input("Enter the y value for point 2: "))
+
+    d = distance(x1, x2, y1, y2)
+
+    print("The distance between the points is: " + str(d))
--- python/software-eng-2/assignment4.py
+++
@@ -1,9 +0,0 @@
-x1 = int(input("Enter the x value for point 1: "))
-y1 = int(input("Enter the y value for point 1: "))
-
-x2 = int(input("Enter the x value for point 2: "))
-y2 = int(input("Enter the y value for point 2: "))
-
-d = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-
-print("The distance between the points is: " + str(d))
